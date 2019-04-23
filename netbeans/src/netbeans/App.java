95ae6a898589e1e8e1e9a3bfac487032196e1834
AndyHu19 <huboxuan@huboxuandeMacBook-Pro.local>
2019-04-23 16:18:43 -0400

    edited it

--- netbeans/src/netbeans/App.java
+++ netbeans/src/netbeans/App.java
@@ -17,6 +17,7 @@ public class App {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
+	System.out.println("I am editing it now.");
     }
     
 }
